e74737870839fc8e14c718fd06550d87345baca3
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2024-01-06 07:21:24 +0530

    authentication completed

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,13 +3,26 @@
 import { authModalState } from '@/atoms/authModalAtom'
 import AuthModal from '@/components/Modals/AuthModal'
 import Navbar from '@/components/Navbar'
-import React from 'react'
+import { auth } from '@/firebase/firebase'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
 import { useRecoilValue } from 'recoil'
 
 type Props = {}
 
 const page = (props: Props) => {
+    const router = useRouter();
     const authModal = useRecoilValue(authModalState)
+    const [user, loading, error] = useAuthState(auth);
+    const [pageLoading, setPageLoading] = useState<boolean>(true)
+
+    useEffect(() => {
+        if (user) router.push("/")
+        if (!loading && !user) setPageLoading(false)
+    }, [user, router, loading])
+
+    if (pageLoading) return null;
 
     return (
         <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -1,5 +1,8 @@
 import { authModalState } from '@/atoms/authModalAtom'
-import React from 'react'
+import { auth } from '@/firebase/firebase'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth'
 import { useSetRecoilState } from 'recoil'
 
 type Props = {}
@@ -9,23 +12,54 @@ const Login = (props: Props) => {
     const handleClick = (type: "login" | "register" | "forgotPassword") => {
         setAuthModalState((prev) => ({ ...prev, type }))
     }
+    const router = useRouter();
+    const [inputs, setInputs] = useState({ email: "", password: "" })
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+    }
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        console.log("hello from handleLogin")
+        e.preventDefault();
+        if (!inputs.email || !inputs.password) return alert("Please fill all fields");
+        try {
+            const newUser = await signInWithEmailAndPassword(inputs.email, inputs.password)
+            if (!newUser) return;
+            router.push("/");
+        } catch (error: any) {
+            alert(error.message)
+        }
+    }
+
+    useEffect(() => {
+        if (error) alert(error.message)
+    }, [error])
 
     return (
-        <form action="" className='space-y-6 px-6 py-4'>
+        <form action="" className='space-y-6 px-6 py-4' onSubmit={handleLogin}>
             <h3 className='text-xl font-medium text-white'>Sign in to LeetClone</h3>
             <div>
                 <label htmlFor="email" className='text-sm font-medium block mb-2 text-gray-300'>
                     Your Email
                 </label>
-                <input type="email" name='email' id='email' className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white" placeholder='name@company.com' />
+                <input onChange={handleInputChange} type="email" name='email' id='email' className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white" placeholder='name@company.com' />
             </div>
             <div>
                 <label htmlFor="password" className='text-sm font-medium block mb-2 text-gray-300'>
                     Your Password
                 </label>
-                <input type="password" name='password' id='password' className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white" placeholder='********' />
+                <input onChange={handleInputChange} type="password" name='password' id='password' className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white" placeholder='********' />
             </div>
-            <button type="submit" className='w-full text-white focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s'>Login</button>
+            <button type="submit" className='w-full text-white focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s'>
+                {loading ? "Loading..." : 'Log In'}
+            </button>
             <button className='flex w-full justify-end' onClick={() => handleClick("forgotPassword")}>
                 <a href="#" className='text-sm block text-brand-orange hover:underline w-full text-right'>
                     Forgot Password?
